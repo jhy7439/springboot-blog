aed1d4af95bbce7b0c4c6b25b4b0d2d0c2701eaf
jhy7439 <jhy7439@naver.com>
2025-08-13 22:30:29 +0900

    회원가입

---
+++ src/main/java/com/haribo/blog/service/UserService.java
@@ -0,0 +1,21 @@
+package com.haribo.blog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.haribo.blog.model.User;
+import com.haribo.blog.repository.UserRepository;
+
+// 스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC를 해준다.
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Transactional
+	public void 회원가입(User user) {
+		userRepository.save(user);
+	}
+}
