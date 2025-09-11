7c12c55c11db6c3b7d24b85916a8c568c57e7493
jhy7439 <jhy7439@naver.com>
2025-09-11 23:37:19 +0900

    스프링 시큐리티 로그인

--- src/main/java/com/haribo/blog/repository/UserRepository.java
+++ src/main/java/com/haribo/blog/repository/UserRepository.java
@@ -4,12 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.haribo.blog.model.User;
+import java.util.List;
+import java.util.Optional;
+
 
 // DAO
 // 자동으로  bean등록이 된다.
 // @Repository // 생략이 가능하다.
 public interface UserRepository extends JpaRepository<User, Integer>{
-	
+	// SELECT * FROM user WHERE username = 1?;
+	Optional<User> findByUsername(String username);
 }
 
 
